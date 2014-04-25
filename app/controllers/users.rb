1b086b87e30a2ef6667cd96f3b54eea7dfbc359f
Christoph Wong & <pair+christophwong.hgw2101@devbootcamp.com>
2014-04-25 16:59:42 -0500

    +Authentication established +Login working +Redirect to Profile page +Sessions Enabled

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -1,12 +1,30 @@
+enable :sessions
+
 get '/users/login' do
   erb :'/users/account_login'
 end
 
 post '/users/login' do
-
-
-
+  user = User.find_by_username(params[:username])
+  p user
+  if user.authenticate(params[:password])
+    session[:user_id] = user.id
+    redirect "/users/#{user.id}/profile"
+  else
+    redirect '/'
+  end
 end
+
+get '/users/logout' do
+  session[:user_id] = nil
+  redirect '/'
+end
+
+get '/users/:id/profile' do
+  @user = User.find(session[:user_id])
+  erb :'/users/profile'
+end
+
 
 get '/users/create_account' do
   erb :'/users/create_account'
@@ -18,4 +36,6 @@ post '/users/create_account' do
            password: params[:password],
            password_confirmation: params[:password_confirmation])
   user.save
+
+  redirect to :'/users/login'
 end
