30fd83d24e286c5453db7bc9be5cfbaadb990cad
Apprentice <apprentice@dbc-chicago-020.local>
2014-04-26 18:26:37 -0500

    Added JS to allow adding of new deck or new cards to deck.  Controller has logic to process this.  decks table now has creator_id field.

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,16 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
+  $("#deck_id").change(function(){
+    var selected = this.value;
+    if(selected == "new_deck"){
+      $('<input class="text-input" type="text" name="custom_deck_id" placeholder="New Deck Name">').insertAfter('#deck_id');
+    }
+  });
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  var cardCount = 2;
+
+  $("#add_nother_card").click(function(){
+    event.preventDefault();
+    $(".new_card_text").append('<div><input class="text-input" type="text" name="question[' + cardCount + ']" placeholder="Question"></div><div><input class="text-input" type="text" name="answer[' + cardCount + ']" placeholder="Answer"></div>');
+    cardCount++
+  })
 });
