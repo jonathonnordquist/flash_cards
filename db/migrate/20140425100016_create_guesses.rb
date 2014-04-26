93d2c2ebc6d861bfdbdb592095275d53ca17eb6f
Apprentice <apprentice@dbc-chicago-022.local>
2014-04-25 19:27:47 -0500

    app views, controllers, css updated

--- db/migrate/20140425100016_create_guesses.rb
+++ db/migrate/20140425100016_create_guesses.rb
@@ -2,7 +2,7 @@ class CreateGuesses < ActiveRecord::Migration
   def change
     create_table :guesses do |t|
       t.integer :round_id, :card_id
-      t.boolean :correct?
+      t.boolean :correct
       t.timestamps
     end
   end
